338b5c7275d29ea789ebd5ee594c8f66991c2457
nikhil153 <nikhil153@gmail.com>
2021-07-27 22:29:43 -0400

    added model weights example

--- lectures/29-July/11-intro-to-machine-learning-part-1/11-ML_Regression_Tutorial.py
+++ lectures/29-July/11-intro-to-machine-learning-part-1/11-ML_Regression_Tutorial.py
@@ -11,8 +11,9 @@
 # a "dummy" model, which always predicts the same thing, regardless of the
 # input features.
 
-
 # +
+import numpy as np
+
 from sklearn import datasets
 from sklearn.linear_model import LinearRegression
 from sklearn.dummy import DummyRegressor
@@ -44,29 +45,57 @@ y_train, y_test = y[:n_train], y[n_train:]
 print(f'n_train samples: {n_train}, n_test samples: {n_test}')
 # -
 
-# We fit the model to the training data.
+# Let's use a linear regression model for our prediction task
 #
 # **Exercise**: what will be the size of the model's coefficients (excluding
 # the intercept)?
 
 model = LinearRegression()
+
+# The initial model intercept (b0) and parameters (aka weights aka coefficients): b1,b2,b3 are randomly assigned. 
+initial_model_params = np.random.uniform(0,1, n_features)
+initial_model_intercept = np.random.uniform(0,1,1)
+
+print(f'\nHere are model parameters before training:\nb0 (intercept): {initial_model_intercept}\n b1-b10: {initial_model_params}')
+
+# What is the MSE right now? 
+def loss_function(X, y, beta):
+    y_pred = np.dot(X,beta[1:]) + beta[0]
+    MSE = np.mean((y_pred - y)**2)
+    return MSE
+
+# MSE with random weights
+beta = np.hstack([initial_model_intercept, initial_model_params])
+initial_MSE = loss_function(X_train, y_train, beta)
+print(f'\nMSE before training (i.e. using random weights): {initial_MSE:.2g}')
+
+# Now we fit the model to the training data.
 model.fit(X_train, y_train)
 train_predictions = model.predict(X_train)
 train_mse = mean_squared_error(y_train, train_predictions)
-print(f"\nMean squared error on train data: {train_mse:.2g}")
 
-# **Exercise**: what function of `X_train`, `y_train`, and the coefficients
-# `beta` does the linear regression minimize in order to estimate `beta`?
-# Implement this function below.
+# +
+# What did model learn? 
+# Let's look at the model parameters (aka weights aka coefficients): b1,b2,b3 ... 
+# Note that sklearn saves the b0 separately as intercept. 
 
+trained_model_params = model.coef_
+trained_model_intercept = model.intercept_
 
-def loss_function(X, y, beta):
-    pass
+print(f'\nHere are model parameters after training:\nb0 (intercept): {trained_model_intercept}\n b1-b10: {trained_model_params}')
 
+# Check if our MSE calculation matches with sklearn! 
+beta = np.hstack([trained_model_intercept, trained_model_params])
+my_train_mse = loss_function(X_train, y_train, beta)
+
+print(f'\nMy calculation of MSE after model training (on train data): {my_train_mse:.2g}')
+print(f"\nScikit-leanr's calculation of MSE after model training (on train data): {train_mse:.2g}")
+
+# -
 
 # Now we evaluate our model on (unseen) test data and display its Mean Squared
 # Error.
-
+print('\nBut what matter is the model performance on the test set!!')
 test_predictions = model.predict(X_test)
 test_mse = mean_squared_error(y_test, test_predictions)
 print(f"Mean squared error on test data: {test_mse:.2g}")
@@ -76,15 +105,14 @@ print(f"Mean squared error on test data: {test_mse:.2g}")
 # this constant value is not arbitrary: it is the one that results in the
 # smallest Mean Squared Error on the training data.
 #
-# **Exercise**: what constant value minimilinezes the MSE for the training sample?
+# **Exercise**: what constant value prediction minimilinezes the MSE for the training sample?
 
 dummy_predictions = DummyRegressor().fit(X_train, y_train).predict(X_test)
 dummy_mse = mean_squared_error(y_test, dummy_predictions)
-print(f"Mean squared error of dummy model on test data: {dummy_mse:.2g}")
+print(f"\nMean squared error of dummy model on test data: {dummy_mse:.2g}")
 
 # What would be ideal predictions? (Impossible in real life!)
 # If we had a oracle predictor - it would predict the true values on the test set perfectly! 
-
 oracle_predictions = y_test
 oracle_mse = mean_squared_error(y_test, oracle_predictions)
 print(f"Mean squared error of oracle model on test data: {oracle_mse:.2g}")
