efc0c87322765ecea23999516ff9b509ae01f76b
nikhil153 <nikhil153@gmail.com>
2021-11-24 21:13:48 -0500

    minor changes to comments for MAIN 2021 workshop

--- lectures/29-July/11-intro-to-machine-learning-part-1/11-ML_Regression_Tutorial.py
+++ lectures/29-July/11-intro-to-machine-learning-part-1/11-ML_Regression_Tutorial.py
@@ -36,7 +36,9 @@ print('- 1) Load dataset ----------------------------------------------------')
 X, y = datasets.load_diabetes(return_X_y=True)
 (n_samples,n_features) = X.shape
 
-print('\nUsing diabetes dataset from scikit-learn:\nhttps://scikit-learn.org/stable/modules/generated/sklearn.datasets.load_diabetes.html#sklearn.datasets.load_diabetes')
+print('\nUsing diabetes dataset from scikit-learn:'
+'\nhttps://scikit-learn.org/stable/modules/generated/sklearn.datasets.load_diabetes.html#sklearn.datasets.load_diabetes')
+
 print(f'\nn_samples: {n_samples}, n_features: {n_features}')
 
 n_train = int(len(y) * 0.8)
@@ -60,25 +62,27 @@ print('\n----------------------------------------------------------------------'
 print('- 2) Try a random model ----------------------------------------------')
 
 # Let start with random initial model intercept (b0) and parameters (aka weights aka coefficients): b1,b2,b3 
-# initial_model_coefs = np.random.uniform(0,1, n_features)
-# initial_model_intercept = np.random.uniform(0,1,1)
-
-# print(f'\nHere are model parameters before training:\nb0 (intercept): {initial_model_intercept}\nb1-b10: {initial_model_coefs}')
+initial_model_coefs = np.random.uniform(0,1, n_features)
+initial_model_intercept = np.random.uniform(0,1,1)
 
 # How do we calculate y_hat?
 # For first sample: y_hat[0] = initial_model_intercept + initial_model_params[0]*X[0,0] + initial_model_params[1]*X[0,1] ... 
 # For second sample: y_hat[1] = initial_model_intercept + initial_model_params[1]*X[1,0] + initial_model_params[1]*X[1,1] ... 
 
+# TODO
 # **Exercise**: write code to calculate y_hat for all training samples! 
-# TODO 
 # y_train_hat = 
-
-# print(f'\nPredicted  (i.e y_hat) length: {len(y_train_hat)}')
 
 # What is the error between model prediction (y_hat) and true values?
 # initial_mse = np.mean((y_train_hat - y_train)**2)
 
+# ------ Uncomment lines in this block to print results ------- 
+
+# print(f'\nThese are the (random) model parameters before training:')
+# print(f'b0 (intercept): {initial_model_intercept}\nb1-b10: {initial_model_coefs}')
 # print(f'\nMSE before training (i.e. using random weights): {initial_mse:.5g}')
+
+# -------------------------------------------------------------
 
 #-
 
@@ -88,10 +92,9 @@ print('- 3) Use scikit-learn to fit the model -------------------------------')
 # Now let's use scikit-learn to fit the model to the training data.
 # +
 
-# model = LinearRegression()
-# model.fit(X_train, y_train)
-# train_predictions = model.predict(X_train)
-# train_mse = mean_squared_error(y_train, train_predictions)
+model = LinearRegression()
+# TODO
+# **Exercise**: fit the model with training data and get the predictions. 
 
 # What did our model learn? 
 # Let's look at the model parameters (aka weights aka coefficients): b1,b2,b3 ... 
@@ -100,29 +103,41 @@ print('- 3) Use scikit-learn to fit the model -------------------------------')
 # trained_model_intercept = model.intercept_
 # trained_model_coef = model.coef_
 
-# print(f'\nHere are model parameters after training:\nb0 (intercept): {trained_model_intercept}\nb1-b10: {trained_model_coef}')
 
-# **Exercise**: Check if our MSE calculation matches with sklearn! 
 # TODO
+# **Exercise**: Check if our MSE calculation matches with sklearn! 
 # y_train_hat = 
 # my_train_mse = 
 
+# ------ Uncomment lines in this block to print results ------- 
+
+# print(f'\nThese are the model parameters after training:')
+# print(f'b0 (intercept): {trained_model_intercept}\nb1-b10: {trained_model_coef}')
 # print(f'\nMy calculation of MSE after model training (on train data): {my_train_mse:.5g}')
 # print(f"\nScikit-learn's calculation of MSE after model training (on train data): {train_mse:.5g}")
 
+# -------------------------------------------------------------
+
 # -
+
 
 print('\n----------------------------------------------------------------------')
 print('- 4) Evaluate our model on (unseen) test data ------------------------')
 
-# Now we evaluate our model on (unseen) test data and display its Mean Squared
-# Error.
+# Now we evaluate our model on (unseen) test data and display its Mean Squared Error.
 # +
 
-# print('\nBut what matter is the model performance on the test set!!')
-# test_predictions = model.predict(X_test)
+# TODO
+# **Exercise**: Check test set performance 
+# test_predictions = 
 # test_mse = mean_squared_error(y_test, test_predictions)
+
+# ------ Uncomment lines in this block to print results ------- 
+
+# print('\nBut what matter is the model performance on the test set!!')
 # print(f"Mean squared error on test data: {test_mse:.5g}")
+
+# -------------------------------------------------------------
 
 print('\n----------------------------------------------------------------------')
 print('- 5) Compare our performance against dummy and oracle models ---------')
@@ -134,6 +149,8 @@ print('- 5) Compare our performance against dummy and oracle models ---------')
 #
 # **Exercise**: what constant value prediction minimizes the MSE for the training sample?
 
+# ------ Uncomment lines in this block to print results ------- 
+
 # dummy_predictions = DummyRegressor().fit(X_train, y_train).predict(X_test)
 # dummy_mse = mean_squared_error(y_test, dummy_predictions)
 # print(f"\nMean squared error of dummy model on test data: {dummy_mse:.5g}")
@@ -143,6 +160,8 @@ print('- 5) Compare our performance against dummy and oracle models ---------')
 # oracle_predictions = y_test
 # oracle_mse = mean_squared_error(y_test, oracle_predictions)
 # print(f"Mean squared error of oracle model on test data: {oracle_mse:.5g}")
+
+# -------------------------------------------------------------
 
 # -
 
@@ -155,6 +174,7 @@ print('- 5) Compare our performance against dummy and oracle models ---------')
 print('\n----------------------------------------------------------------------')
 print('- 6) Make Plots! -----------------------------------------------------')
 
+# ------ Uncomment lines in this block to print results ------- 
 # plt.plot(
 #     [y_test.min(), y_test.max()],
 #     [y_test.min(), y_test.max()],
@@ -176,6 +196,9 @@ print('- 6) Make Plots! -----------------------------------------------------')
 # plt.gca().set_ylabel("Predicted outcome")
 # plt.gca().set_title("True and predicted diabetes progress")
 # plt.show()
+
+# -------------------------------------------------------------
+
 
 # Here we have a small number of features that are not too correlated
 # (condition number of `X_train` is 23), so linear regression without
